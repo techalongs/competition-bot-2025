af2533849297303a9942133e4fa079e73d293443
Dave <dave@davemx.com>
2025-10-05 11:11:56 -0700

    Added Eleanor's code from the other repo. Moved into packages so it's not all piled in.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RobotCentricDrive.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@TeleOp
+public class RobotCentricDrive extends OpMode {
+    private GamepadEx driver1;
+    private GamepadEx driver2;
+    private Robot robot;
+
+    @Override
+    public void init() {
+        driver1 = new GamepadEx(gamepad1);
+        driver2 = new GamepadEx(gamepad2);
+        robot = new Robot(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        robot.drive(Robot.DriveState.ROBOT_CENTRIC, driver1, 0.5);
+    }
+
+}
