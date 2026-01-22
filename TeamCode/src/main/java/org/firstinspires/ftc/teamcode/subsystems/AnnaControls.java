1ff7b1c8cb18d2408991035aab7189a68da56c68
Anna <anna@annamx.com>
2026-01-21 19:35:39 -0700

    Partly added AnnaControls but there are still wrong stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AnnaControls.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.ConditionalCommand;
+import com.seattlesolvers.solverslib.command.DeferredCommand;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
+import com.seattlesolvers.solverslib.gamepad.ToggleButtonReader;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.util.REVColorSensor;
+
+import java.util.Arrays;
+
+public class AnnaControls extends SubsystemBase {
+
+    private GamepadEx driver1;
+    private GamepadEx driver2;
+    private Robot robot;
+    private ToggleButtonReader toggleDriveSlow;
+    private ToggleButtonReader toggleFieldCentric;
+    private boolean intakeState = false;
+    private REVColorSensor sensor1;
+    private REVColorSensor sensor2;
+    private double driveFastSpeedLimit = 1.0;
+    private double driveSlowSpeedLimit = 0.5;
+    private Launcher.Power launcherPower = Launcher.Power.SHORT;
+    private final Launcher.Power[] launcherPowers =
+            new Launcher.Power[]{Launcher.Power.LONG, Launcher.Power.MID, Launcher.Power.SHORT};
+    private int launcherState = 2;
+    private final int[][] gamepadColors = new int[][]{{255, 0, 0}, {0, 0, 255}, {0, 255, 0}};
+
+
+    public AnnaControls(GamepadEx driver1, GamepadEx driver2, Robot robot, Robot robot1, CommandScheduler commandScheduler, REVColorSensor sensor1, REVColorSensor sensor2) {
+
+        this.driver1 = driver1;
+        this.driver2 = driver2;
+        this.robot = robot1;
+        this.sensor1 = sensor1;
+        this.sensor2 = sensor2;
+
+
+        // With multi button toggles there is and enter before the .and
+        //  Drive Slow Toggle = Options + Left Bumper
+        toggleDriveSlow = new ToggleButtonReader(driver1.getGamepadButton(GamepadKeys.Button.OPTIONS)
+                .and(driver1.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER))::get);
+
+        // Drive Field Centric Toggle = Options + X
+        toggleFieldCentric = new ToggleButtonReader(driver1.getGamepadButton(GamepadKeys.Button.OPTIONS)
+                .and(driver1.getGamepadButton(GamepadKeys.Button.SQUARE))::get);
+
+
+        // Launcher Power Toggle - Right Bumper
+        driver2.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
+                .whenPressed(
+                        new InstantCommand(() -> {
+                            int index = ++launcherState % launcherPowers.length;
+                            launcherPower = launcherPowers[index];
+                            driver2.gamepad.setLedColor(
+                                    gamepadColors[index][0],
+                                    gamepadColors[index][1],
+                                    gamepadColors[index][2],
+                                    5000);
+                        })
+                );
+
+        // Intake - Y
+        driver1.getGamepadButton(GamepadKeys.Button.TRIANGLE)
+                .whenPressed(new ConditionalCommand(
+                        robot.runIntake(),
+                        robot.stopIntake(),
+                        () -> {
+                            intakeState = !intakeState;
+                            return intakeState;
+                        }
+                ));
+
+        // Launchers - X
+        driver2.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
+                .whenPressed(new DeferredCommand(() -> robot.launchColor(Launcher.Color.PURPLE, launcherPower), null));
+        driver2.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER)
+                .whenPressed(new DeferredCommand(() -> robot.launchColor(Launcher.Color.GREEN, launcherPower), null));
+        driver2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER).(new DeferredCommand(() -> robot.launchAll(launcherPower), null));
+
+        driver2.getGamepadButton(GamepadKeys.Button.DPAD_LEFT)
+                .whenPressed(new DeferredCommand(() -> robot.launchLeft(launcherPower), null));
+        driver2.getGamepadButton(GamepadKeys.Button.DPAD_UP)
+                .whenPressed(new DeferredCommand(() -> robot.launchMid(launcherPower), null));
+        driver2.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT)
+                .whenPressed(new DeferredCommand(() -> robot.launchRight(launcherPower), null));
+    }
+
+}
