83bfab7de6acf4a7de1417b4ca4531576676cb5a
codagirlaz <eleanor_lam@outlook.com>
2026-02-15 14:44:20 -0700

    Changed plinko fork

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestingFork.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestingFork.java
@@ -9,7 +9,7 @@ public class TestingFork extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Servo fork = hardwareMap.get(Servo.class, "intakeFork");
+        Servo rightFork = hardwareMap.get(Servo.class, "leftFork");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -17,11 +17,11 @@ public class TestingFork extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            if (gamepad1.square) fork.setPosition(0);
-            else if (gamepad1.circle) fork.setPosition(1);
-            else if (gamepad1.cross) fork.setPosition(0.4);
+            if (gamepad1.square) rightFork.setPosition(0);
+            else if (gamepad1.circle) rightFork.setPosition(0.6);
+            else if (gamepad1.cross) rightFork.setPosition(0.4);
 
-            telemetry.addData("Position", fork.getPosition());
+            telemetry.addData("Position", rightFork.getPosition());
             telemetry.addData("Status", "Active");
             telemetry.update();
         }
