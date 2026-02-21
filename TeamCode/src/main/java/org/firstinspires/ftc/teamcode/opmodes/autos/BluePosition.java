36477576b53ce04ec0b953c4c18eadbe2832fdc7
codagirlaz <eleanor_lam@outlook.com>
2026-02-21 07:28:01 -0700

    Finalized positions (Close)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/BluePosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/BluePosition.java
@@ -3,19 +3,19 @@ package org.firstinspires.ftc.teamcode.opmodes.autos;
 import com.pedropathing.geometry.Pose;
 
 public enum BluePosition {
-    SHORT_START(new Pose(18, 121, Math.toRadians(140))),
+    SHORT_START(new Pose(20, 123, Math.toRadians(140))),
     LONG_START(new Pose(56, 8, Math.toRadians(90))),
     SHORT_SHOOT(new Pose(49, 95, Math.toRadians(135))),
     LONG_SHOOT(new Pose(56, 36, Math.toRadians(67))),
     SHORT_COLLECT_PREP(new Pose(47, 84, Math.toRadians(180))),
-    SHORT_COLLECT(new Pose(17, 84, Math.toRadians(180))),
+    SHORT_COLLECT(new Pose(15, 84, Math.toRadians(180))),
     MID_COLLECT_PREP(new Pose(45, 60.5, Math.toRadians(180))),
-    MID_COLLECT(new Pose(15, 60.5, Math.toRadians(180))),
+    MID_COLLECT(new Pose(13, 60.5, Math.toRadians(180))),
     LONG_COLLECT_PREP(new Pose(49, 37, Math.toRadians(180))),
-    LONG_COLLECT(new Pose(24, 37, Math.toRadians(180))),
+    LONG_COLLECT(new Pose(20, 37, Math.toRadians(180))),
     DUMP_PREP(new Pose(36, 73, Math.toRadians(90))),
     DUMP(new Pose(16, 68, Math.toRadians(90))),
-    SHORT_END(new Pose(32, 85, Math.toRadians(0))),
+    SHORT_END(new Pose(32, 85, Math.toRadians(180))),
     LONG_END(new Pose(39, 16, Math.toRadians(0)));
 
     public final Pose pos;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/RedPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/RedPosition.java
@@ -3,16 +3,16 @@ package org.firstinspires.ftc.teamcode.opmodes.autos;
 import com.pedropathing.geometry.Pose;
 
 public enum RedPosition {
-    SHORT_START(new Pose(126, 121, Math.toRadians(40))),
+    SHORT_START(new Pose(128, 123, Math.toRadians(40))),
     LONG_START(new Pose(88, 8, Math.toRadians(90))),
     SHORT_SHOOT(new Pose(95, 95, Math.toRadians(45))),
     LONG_SHOOT(new Pose(56, 36, Math.toRadians(67))),
-    SHORT_COLLECT_PREP(new Pose(95, 82, Math.toRadians(0))),
-    SHORT_COLLECT(new Pose(120, 82, Math.toRadians(0))),
-    MID_COLLECT_PREP(new Pose(95, 57, Math.toRadians(0))),
-    MID_COLLECT(new Pose(118, 57, Math.toRadians(0))),
-    LONG_COLLECT_PREP(new Pose(95, 33, Math.toRadians(0))),
-    LONG_COLLECT(new Pose(120, 33, Math.toRadians(0))),
+    SHORT_COLLECT_PREP(new Pose(95, 84, Math.toRadians(0))),
+    SHORT_COLLECT(new Pose(122, 84, Math.toRadians(0))),
+    MID_COLLECT_PREP(new Pose(95, 60, Math.toRadians(0))),
+    MID_COLLECT(new Pose(122, 60, Math.toRadians(0))),
+    LONG_COLLECT_PREP(new Pose(95, 37, Math.toRadians(0))),
+    LONG_COLLECT(new Pose(124, 37, Math.toRadians(0))),
     DUMP_PREP(new Pose(118, 77, Math.toRadians(90))),
     DUMP(new Pose(128, 77, Math.toRadians(90))),
     SHORT_END(new Pose(112, 85, Math.toRadians(0))),
